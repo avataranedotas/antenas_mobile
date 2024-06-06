57dbe66c0e4c09172eb66461ae9ebcd92ba4e909
Alexandre Moleiro <avataranedotas@yahoo.com>
2024-06-06 17:41:14 +0100

    Update post.sh

--- post.sh
+++ post.sh
@@ -5,3 +5,7 @@ osmtogeojson digi.json >| digi.geojson
 osmtogeojson nos.json >| nos.geojson
 osmtogeojson meo.json >| meo.geojson
 osmtogeojson faltam.json >| faltam.geojson
+osmtogeojson digi_investigar.json >| digi_investigar.geojson
+osmtogeojson digi_ferros.json >| digi_ferros.geojson
+osmtogeojson digi_descobertas.json >| digi_descobertas.geojson
+osmtogeojson digi_existentes.json >| digi_existentes.geojson
