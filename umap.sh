850f85a8dbf49c6cec16d8c577bcde01c5527ad3
Alexandre Moleiro <avataranedotas@yahoo.com>
2024-06-08 19:01:03 +0100

    Update umap.sh

--- umap.sh
+++ umap.sh
@@ -51,7 +51,12 @@ echo "Caça às DIGI - Existentes não Vantage"
 wget -O digi_existentes.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A60%5D%3Barea(id%3A3600295480)-%3E.searchArea%3B(node%5B%22communication%3Amobile_phone%22~%22.%22%5D%5B%22MNC%22!~%2202%22%5D%5B%22owner%22!%3D%22Vantage%20Towers%22%5D%5B%22MNC%22!%3D%2201%22%5D%5B%22communication%3Amobile_phone%3Aextra_tube%22!~%22.%22%5D%5B%22location%22!%3D%22indoor%22%5D(area.searchArea)%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B%0A'
 sleep 60
 
-#Partilhadas
+#Ran-sharing
+#echo "Antenas Ran-sharing"
+wget -O ran_sharing.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A60%5D%3Barea(id%3A3600295480)-%3E.searchArea%3B(%0Anode%5B%22communication%3Amobile_phone%3Aran_sharing%22~%22.%22%5D(area.searchArea)%3B%0A)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B'
+sleep 60
+
+#Partilhadas ------------ não usado
 #echo "Partilhadas"
 #wget -O partilhadas.json 'https://maps.mail.ru/osm/tools/overpass/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea(id%3A3600295480)-%3E.searchArea%3B(node%5B%22communication%3Amobile_phone%22%3D%22yes%22%5D%5B%22MCC%22%3D%22268%22%5D%5B%22MNC%22!%3D%2201%22%5D%5B%22MNC%22!%3D%2203%22%5D%5B%22MNC%22!%3D%2206%22%5D(area.searchArea)%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B'
 #sleep 60
