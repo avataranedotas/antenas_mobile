350f388af292efd0efda3ef39cbf6e41680b1b9d
Alexandre Moleiro <avataranedotas@yahoo.com>
2025-09-08 15:19:27 +0100

    Update umap.sh

--- umap.sh
+++ umap.sh
@@ -67,7 +67,7 @@ sleep 60
 
 #todos_sites
 echo "Todos os Sites activos"
-wget -O ran_sharing.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A60%5D%3Barea(id%3A3600295480)-%3E.searchArea%3B(%0Anode%5B%22communication%3Amobile_phone%22%3D%22yes%22%5D(area.searchArea)%3B%0A)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B'
+wget -O todos.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A60%5D%3Barea(id%3A3600295480)-%3E.searchArea%3B(%0Anode%5B%22communication%3Amobile_phone%22%3D%22yes%22%5D(area.searchArea)%3B%0A)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B'
 sleep 60
 
 
